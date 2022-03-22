3a78de4886aa0b8f5f4006eaa6dd72c533343388
DaiNiusCer <d.cerkauskas@gmail.com>
2022-03-22 11:33:12 +0200

    8 užduotis atlikta

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,32 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  //sum() - priima du skaičius ir grąžina jų sumą;
+  sum = (a, b) => {
+    let answer = a + b
+    return answer
+  }
+  //subtraction() - priima du skaičius ir grąžina jų skirtumą;
+  subtraction = (a, b) => {
+    let answer = a - b
+    return answer
+  }
+  //multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
+  multiplication = (a, b) => {
+    let answer = a * b
+    return answer
+  }
+  //division() - priima du skaičius ir grąžina jų dalybos rezultatą;
+  division = (a, b) => {
+    let answer = a / b
+    return answer
+  }
+}
+
+let skaiciuotuvas = new Calculator
+
+console.log(skaiciuotuvas.sum(6, 2))
+console.log(skaiciuotuvas.subtraction(6, 2))
+console.log(skaiciuotuvas.multiplication(6, 2))
+console.log(skaiciuotuvas.division(6, 2))
