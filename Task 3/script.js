c8e3b962431ab6e2832617726d4a8a02360bd14a
DaiNiusCer <d.cerkauskas@gmail.com>
2022-03-22 09:45:29 +0200

    3 užduotis atlikta

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,33 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+document.querySelector("#btn").addEventListener("click", (e) => {
+  console.log(e)
+  fetch(ENDPOINT)
+    .then(res => res.json())
+    .then(data => data)
+    //data => console.log(data)
+    .then(data => {
+      let isvedimas = ""
+
+      data.forEach(item => {
+        isvedimas += `
+        <div class="mainUsers">
+<div class="users"> <img src="${item.avatar_url}" alt=""</div>
+<br>
+<div class="login"><p>Login: ${item.login} </p></div>
+</div>
+`
+        document.querySelector("#output").innerHTML = isvedimas
+
+      })
+
+
+    })
+
+  let mygtukas = document.querySelector("#btn");
+  mygtukas.setAttribute("class", "hidden")
+
+
+})
