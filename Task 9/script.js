45b437fedc152b67290eae2f9b1cc5da8d44fae0
DaiNiusCer <d.cerkauskas@gmail.com>
2022-03-22 11:52:43 +0200

    9 užduotis atlikta

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,28 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive = () => {
+    let atsakymas = (this.budget > 100000000)
+    return atsakymas
+  }
+
+
+
+
+}
+
+let naujasFilmas = new Movie("Spider-Man", "Sam-Raimi", 100000001)
+let naujasFilmas1 = new Movie("Rush Hour", "Brett Ratner", 950000)
+
+console.log(naujasFilmas)
+console.log(naujasFilmas.wasExpensive())
+
+console.log(naujasFilmas1)
+console.log(naujasFilmas1.wasExpensive())
